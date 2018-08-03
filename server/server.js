d9a71daca3ba619a4d833f607bfd6b59e68c89ec
dennis cheng <c_n_cheng@yahoo.com>
2018-08-03 17:21:55 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(publicPath));
+
+app.get('/', (req,res)=>{
+    res.send('anything');
+})
+
+app.listen(port, ()=>{
+    console.log(`Server is up on port ${port}...`);
+});
+
+module.exports = {
+    app
+};
