9aae98f7276c705b5d7a68669f639c29daaa0286
dennis cheng <c_n_cheng@yahoo.com>
2018-08-21 09:04:29 -0500

    Add connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -9,21 +9,26 @@ const port = process.env.PORT || 3000;
 // https://guarded-wildwood-13904.herokuapp.com/
 
 var app = express();
+
 // create a http server
 var server = http.createServer(app);
+
 // create a web socket server
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
+// Listen to new connection event
 io.on('connection', (socket)=> {
     console.log('New user connected');
 
+    // Listen to socket disconnect event
     socket.on('disconnect', ()=>{
         console.log('Disconnect user client');
     });
 });
 
+// set http server to listen to port 
 server.listen(port, ()=>{
     console.log(`Server is up on port ${port}...`);
 });
