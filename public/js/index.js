29916176ae99f07b997e5e910747150ea9bc8255
dennis cheng <c_n_cheng@yahoo.com>
2018-08-23 17:02:09 -0500

    add autoscrolling

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,21 @@
 // Create a socket connection
 var socket = io();
 
+function scrollToBottom () {
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    };
+};
+
 // Listen to 'connect' event
 socket.on('connect', function () {
     console.log('Connected to server');
@@ -21,6 +36,7 @@ socket.on('newMessage', function (message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     //
     // Codes before using Mustache for templating
@@ -42,6 +58,7 @@ socket.on('newLocationMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
     //
     // Codes before using Mustache templating
